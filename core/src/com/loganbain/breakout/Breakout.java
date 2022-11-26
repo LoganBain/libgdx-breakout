a2a0cff6d373da846194f5e18571076611f985c2
LoganBain <loganbain23@gmail.com>
2022-11-26 12:34:52 +0000

    Testing shape renderer

--- core/src/com/loganbain/breakout/Breakout.java
+++ core/src/com/loganbain/breakout/Breakout.java
@@ -1,3 +1,18 @@
 package com.loganbain.breakout;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+public class Breakout extends ApplicationAdapter {
+    ShapeRenderer shape;
+
+    public void create() {
+        shape = new ShapeRenderer();
+    }
+
+    public void render() {
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        shape.circle(50, 50, 50);
+        shape.end();
+    }
+}
