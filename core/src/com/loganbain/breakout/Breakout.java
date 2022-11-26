cbf1c79fb190932893a23987e369ec95e9adffe0
LoganBain <loganbain23@gmail.com>
2022-11-26 13:03:29 +0000

    Created a random bouncing ball generator

--- core/src/com/loganbain/breakout/Breakout.java
+++ core/src/com/loganbain/breakout/Breakout.java
@@ -5,9 +5,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Breakout extends ApplicationAdapter {
     ShapeRenderer shape;
-    Ball ball;
+    ArrayList<Ball> balls;
 
     int x = 50;
     int xSpeed = 5;
@@ -15,16 +18,31 @@ public class Breakout extends ApplicationAdapter {
 
     public void create() {
         shape = new ShapeRenderer();
-        ball = new Ball(150, 200, 70, 12, 5);
+        balls = new ArrayList<>();
+        Random r = new Random();
+
+        // create 10 randomly sized and located balls
+        for (int i = 0; i < 10; i++) {
+            balls.add(new Ball(
+                    r.nextInt(Gdx.graphics.getWidth()), // random x location up to application width
+                    r.nextInt(Gdx.graphics.getHeight()), // random y location up to application height
+                    r.nextInt(100), // random circle radius up to 100
+                    r.nextInt(15), // random xSpeed up to 15
+                    r.nextInt(15))); // random ySpeed up to 15
+        }
+
+        // ball = new Ball(150, 200, 70, 12, 5);
     }
 
     public void render() {
         // Clears the screen (avoids a "trail" from drawing the circle)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        ball.update();
         shape.begin(ShapeRenderer.ShapeType.Filled);
-        ball.draw(shape);
+        for (Ball ball : this.balls) {
+            ball.update();
+            ball.draw(shape);
+        }
         shape.end();
     }
 }
